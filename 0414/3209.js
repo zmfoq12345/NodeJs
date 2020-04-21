32388c266989c7189d3b9e94315aacc1b1e835c0
zmfoq12345 <anihyun1026@naver.com>
2020-04-21 11:02:52 +0900

    0421_2End

--- 0414/3209.js
+++ 0414/3209.js
@@ -4,8 +4,6 @@ var fs = require('fs');
 // 파일 처리와 관련된 모듈
 var url = require('url');
 // 쿼리스트링을 분석 / 분리하여 문자열로 변환하는 모듈
-var queryString = require('query-string');
-
 
 http.createServer(function(req, res){
     var path = url.parse(req.url, true).pathname;
@@ -31,22 +29,16 @@ http.createServer(function(req, res){
                 res.end(txt);
             }
         });
-    }else if(path === './txtFile/table'){
-        const queryObject = queryString.parse(query);
+    }else if(path === '/table'){
         table = ''
-        for(i=1;i<=parseInt(queryObject.id);i++){
-            table+='<th>'+i.toString();
+        for(var i=1;i<=parseInt(query.id);i++){
+            table+=`<th>${i}`
         }
-        res.end(`<html>
-            <head>
-            <meta charset='utf-8'>
-            </head>
-                <body>
-                    <table border="1">
-                        ${table}
-                    </table>
-                </body>
-            </html>`)
+        res.end(`
+            <table border=1>
+                ${table}
+            </table>
+        `)
     }else{
         fs.readFile('./txtFile/other','utf-8', function(err, txt){
             if (err){
