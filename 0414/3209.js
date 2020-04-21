da125744f34672bd87349bec8f5b7e415e03da92
zmfoq12345 <anihyun1026@naver.com>
2020-04-21 09:54:55 +0900

    0421_1End

--- 0414/3209.js
+++ 0414/3209.js
@@ -1,12 +1,21 @@
 var http = require('http');
-var fs = require('fs');
-var queryString = require('query-string');
 // 로컬IP : 127.0.0.1 / localhost
+var fs = require('fs');
+// 파일 처리와 관련된 모듈
+var url = require('url');
+// 쿼리스트링을 분석 / 분리하여 문자열로 변환하는 모듈
+var queryString = require('query-string');
+
 
 http.createServer(function(req, res){
-    var _url = req.url.split('?');
-    var reqUrl = _url[0]
-    if(reqUrl === '/html'){
+    var path = url.parse(req.url, true).pathname;
+    var query = url.parse(req.url, true).query;
+
+    console.log(req.url); // url
+    console.log(path); // url에서 분석한 파일명
+    console.log(query.id); // 쿼리스트링의 데이터
+
+    if(path === '/html'){
         fs.readFile('./txtFile/html','utf-8', function(err, txt){
             if (err){
                 console.log(err.message);
@@ -14,7 +23,7 @@ http.createServer(function(req, res){
                 res.end(txt);
             }
         });
-    }else if(reqUrl === '/nodejs'){
+    }else if(path === '/nodejs'){
         fs.readFile('./txtFile/nodejs','utf-8', function(err, txt){
             if (err){
                 console.log(err.message);
@@ -22,8 +31,8 @@ http.createServer(function(req, res){
                 res.end(txt);
             }
         });
-    }else if(reqUrl === './txtFile/table'){
-        const queryObject = queryString.parse(_url[1]);
+    }else if(path === './txtFile/table'){
+        const queryObject = queryString.parse(query);
         table = ''
         for(i=1;i<=parseInt(queryObject.id);i++){
             table+='<th>'+i.toString();
